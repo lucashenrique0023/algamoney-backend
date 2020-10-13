b9d11851fa5d4837db04a0054d8789e10c1c897b
detectiverust93 <lucas.henrique0023@gmail.com>
2020-10-13 20:09:35 -0300

    Usando Eventos do Spring para Adicionar Header no retorno da requisicao.
    Utilizamos uma publicacao de evento para sinalizar a aplicacao de quando
    um novo recurso for criado no sistema, para reduzir codigo duplicado e
    organizar a logica da aplicacao.
    
    1. Criamos um Evento chamado RecursoCriadoEvent que estende a classe
    ApplicationEvent.
    
    2. Criamos uma classe para escutar o evento e realizar alguma logica
    chamada RecursoCriadoListener que implementa ApplicationListener e
    monitora um evento especifico.
    
    3. Dentro do listener implementamos o metodo com uma logica para inserir
    o header na requisicao de resposta.
    
    4. Por fim configuramos os resources para emitirem o evento quando um
    novo recurso for criado.

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.Repository.CategoriaRepository;
+import com.algamoney.api.event.RecursoCriadoEvent;
 import com.algamoney.api.model.Categoria;
 
 @RestController
@@ -26,6 +27,9 @@ public class CategoriaResource {
 	
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Categoria> listar() {
@@ -36,12 +40,8 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response){
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(categoriaSalva);	
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
--- src/main/java/com/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algamoney/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.Repository.PessoaRepository;
+import com.algamoney.api.event.RecursoCriadoEvent;
 import com.algamoney.api.model.Pessoa;
 
 @RestController
@@ -26,6 +27,9 @@ public class PessoaResource {
 	
 	@Autowired
 	PessoaRepository pessoaRepository;
+	
+	@Autowired
+	ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Pessoa> listar() {
@@ -46,12 +50,8 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 
